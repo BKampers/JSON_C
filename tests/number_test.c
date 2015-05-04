8bb97fa436d816c93fd61657445842e31bf2b156
BKampers <github@bartkampers.nl>
2015-05-04 22:51:57 +0200

    Test more integers

--- tests/number_test.c
+++ tests/number_test.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "JsonParser.h" 
 
@@ -9,53 +10,125 @@
 */
 
 
-void validIntegerTest() {
-    JsonNode object, pair, value;
-    printf("JSON validIntegerTest\n");
-    Initialize("{\"int\":123}", &object);
-    ParseFirst(&object, &pair);
-    GetValue(&pair, &value);
-    if ((value.type == JSON_NUMBER) && (strncmp(value.source + value.offset, "123", value.length) == 0))
-    {
-        printf("%%TEST_PASSED%% time=0 testname=validIntegerTest (JSON Parser Test) message=OK\n");
-    }
-    else
-    {
-        printf("%%TEST_FAILED%% time=0 testname=validIntegerTest (JSON Parser Test) message=Error\n");
-    }
+const char* testName = NULL;
+
+
+void StartTest(const char* name)
+{
+    testName = name;
+    printf("%%TEST_STARTED%% ");
+    printf(testName);
+    printf(" (JSON Parser Test)\n");
 }
 
 
-void invalidIntegerTest() {
+void FinishTest()
+{
+    printf("%%TEST_FINISHED%% time=0 ");
+    printf(testName);
+    printf(" (JSON Parser Test)\n");
+    testName = NULL;
+}
+
+
+void PrintTestResult(bool pass, const char* message)
+{
+    if (pass)
+    {
+        printf("%%TEST_PASSED%%");
+    }
+    else
+    {
+        printf("%%TEST_FAILED%%");
+    }
+    printf(" time=0 testname=");
+    printf(testName);
+    printf(" (JSON Parser Test)");
+    if (strlen(message) > 0)
+    {
+        printf(" message=");
+        printf(message);
+    }
+    printf("\n");
+}
+
+
+void ExpectEqualInteger(int expected, int actual)
+{
+    char message[256];
+    bool pass = expected == actual;
+    if (pass)
+    {
+        message[0] = '\0';
+    }
+    else
+    {
+        sprintf(message, "expected = %d, actual = %d", expected, actual);
+    }
+    PrintTestResult(pass, testName);
+}
+
+
+void ExpectEqualString(const char* expected, const JsonNode* actual)
+{
+    char message[256];
+    bool pass = (strlen(expected) == actual->length) &&(strncmp(expected, actual->source + actual->offset, actual->length) == 0);
+    if (pass)
+    {
+        message[0] = '\0';
+    }
+    else
+    {
+        sprintf(message, "expected = %s, actual = ", expected);
+        strncat(message, actual->source + actual->offset, actual->length);
+    }
+    PrintTestResult(pass, testName);
+}
+
+
+void ValidIntegerTest() {
     JsonNode object, pair, value;
-    printf("JSON invalidIntegerTest\n");
-    Initialize("{\"int\":0123}", &object);
-    if (object.type == JSON_INVALID)
-    {
-        printf("%%TEST_PASSED%% time=0 testname=invalidIntegerTest (JSON Parser Test) message=Object OK\n");
-    }
-    else
-    {
-        printf("%%TEST_FAILED%% time=0 testname=invalidIntegerTest (JSON Parser Test) message=Object error\n");
-    }
+    Initialize("{\"int\":123}", &object);
     ParseFirst(&object, &pair);
-    if (pair.type == JSON_INVALID)
-    {
-        printf("%%TEST_PASSED%% time=0 testname=invalidIntegerTest (JSON Parser Test) message=Pair OK\n");
-    }
-    else
-    {
-        printf("%%TEST_FAILED%% time=0 testname=invalidIntegerTest (JSON Parser Test) message=Pair error\n");
-    }
     GetValue(&pair, &value);
-    if (value.type == JSON_INVALID)
-    {
-        printf("%%TEST_PASSED%% time=0 testname=invalidIntegerTest (JSON Parser Test) message=Value OK\n");
-    }
-    else
-    {
-        printf("%%TEST_FAILED%% time=0 testname=invalidIntegerTest (JSON Parser Test) message=Value error\n");
-    }
+    ExpectEqualInteger(JSON_NUMBER, value.type);
+    ExpectEqualString("123", &value);
+    Initialize("{\"int\":9}", &object);
+    ParseFirst(&object, &pair);
+    GetValue(&pair, &value);
+    ExpectEqualInteger(JSON_NUMBER, value.type);
+    ExpectEqualString("9", &value);
+    Initialize("{\"int\":0}", &object);
+    ParseFirst(&object, &pair);
+    GetValue(&pair, &value);
+    ExpectEqualInteger(JSON_NUMBER, value.type);
+    ExpectEqualString("0", &value);
+    Initialize("{\"int\":-0}", &object);
+    ParseFirst(&object, &pair);
+    GetValue(&pair, &value);
+    ExpectEqualInteger(JSON_NUMBER, value.type);
+    ExpectEqualString("-0", &value);
+    Initialize("{\"int\":-8}", &object);
+    ParseFirst(&object, &pair);
+    GetValue(&pair, &value);
+    ExpectEqualInteger(JSON_NUMBER, value.type);
+    ExpectEqualString("-8", &value);
+    Initialize("{\"int\":-75}", &object);
+    ParseFirst(&object, &pair);
+    GetValue(&pair, &value);
+    ExpectEqualInteger(JSON_NUMBER, value.type);
+    ExpectEqualString("-75", &value);
+}
+
+
+void InvalidIntegerTest() {
+    JsonNode object;
+    Initialize("{\"int\":12a}", &object);
+    ExpectEqualInteger(JSON_INVALID, object.type);
+    Initialize("{\"int\":0123}", &object);
+    ExpectEqualInteger(JSON_INVALID, object.type);
+    Initialize("{\"int\":+5}", &object);
+    ExpectEqualInteger(JSON_INVALID, object.type);
 }
 
 
@@ -63,13 +136,13 @@ int main(int argc, char** argv) {
     printf("%%SUITE_STARTING%% JSON Parser Test\n");
     printf("%%SUITE_STARTED%%\n");
 
-    printf("%%TEST_STARTED%% validIntegerTest (JSON Parser Test)\n");
-    validIntegerTest();
-    printf("%%TEST_FINISHED%% time=0 validIntegerTest (JSON Parser Test) \n");
+    StartTest("ValidIntegerTest");
+    ValidIntegerTest();
+    FinishTest();
 
-    printf("%%TEST_STARTED%% invalidIntegerTest (JSON Parser Test)\n");
-    invalidIntegerTest();
-    printf("%%TEST_FINISHED%% time=0 invalidIntegerTest (JSON Parser Test) \n");
+    StartTest("InvalidIntegerTest");
+    InvalidIntegerTest();
+    FinishTest();
 
     printf("%%SUITE_FINISHED%% time=0\n");
 
